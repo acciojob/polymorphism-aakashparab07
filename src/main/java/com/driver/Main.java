03ac012d4cb2e0a36541f30e39ec39c5f1a1108e
91961 <aakashparab98@gmail.com>
2023-04-04 18:53:30 +0530

    1st Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
+    public static void main(String []args)
+    {
+        Product p=new Product();
 
+        int product1 = p.product(10,5);
+        int product2 = p.product(10,5,7);
+        double productBool = p.product(27.0,35.0);
+
+        System.out.println(product1+" "+product2+" "+productBool);
+
+    }
+    public static class Product
+    {
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
 }
